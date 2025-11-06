a2b5db9af61bf1df73ed91e20ae44ae53c5eb183
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2025-11-05 22:05:18 -0500

    first commit

---
+++ src/main/java/com/ufps/schedulequery/ScheduleQueryMsvcApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.schedulequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleQueryMsvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleQueryMsvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufps/schedulequery/ScheduleQueryMsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.schedulequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleQueryMsvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
